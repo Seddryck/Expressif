cfbbfc5e7fa740e6ac2ad26fbfbc09a79e248e8d
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-11-13 00:21:20 +0100

    Implement first set of functions for special values

---
+++ Expressif.Testing/Functions/Special/SpecialTransformationsTest.cs
@@ -0,0 +1,53 @@
+﻿using Expressif.Functions.Special;
+using Expressif.Values.Special;
+
+namespace Expressif.Testing.Functions.Special
+{
+    [TestFixture]
+    public class SpecialTransformationsTest
+    {
+
+        [Test]
+        [TestCase("foo")]
+        [TestCase("(any)")]
+        [TestCase("(empty)")]
+        [TestCase("(blank)")]
+        [TestCase("(value)")]
+        [TestCase("(null)")]
+        [TestCase(null)]
+        [TestCase(150)]
+        public void AnyToAny_Any(object value)
+            => Assert.That(new AnyToAny().Evaluate(value), Is.EqualTo(new Any()));
+
+
+        [Test]
+        [TestCase("foo")]
+        [TestCase("(any)")]
+        [TestCase("(empty)")]
+        [TestCase("(blank)")]
+        [TestCase("(value)")]
+        [TestCase(150)]
+        public void ValueToValue_NotNull_Value(object value)
+            => Assert.That(new ValueToValue().Evaluate(value), Is.EqualTo(new Value()));
+
+        [Test]
+        [TestCase("(null)")]
+        [TestCase(null)]
+        public void ValueToValue_Null_Null(object value)
+            => Assert.That(new ValueToValue().Evaluate(value), Is.EqualTo(new Null()));
+
+        [Test]
+        [TestCase("(null)")]
+        [TestCase(null)]
+        public void NullToValue_Null_Value(object value)
+            => Assert.That(new NullToValue().Evaluate(value), Is.EqualTo(new Value()));
+
+        [Test]
+        [TestCase("foo")]
+        [TestCase("(empty)")]
+        [TestCase("(blank)")]
+        [TestCase("(value)")]
+        public void NullToValue_NotNull_Unchanged(object value)
+            => Assert.That(new NullToValue().Evaluate(value), Is.EqualTo(new Value()));
+    }
+}
---
+++ Expressif/Functions/Special/SpecialTransformations.cs
@@ -0,0 +1,94 @@
+﻿using Expressif.Values;
+using Expressif.Values.Casters;
+using Expressif.Values.Special;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Expressif.Functions.Special
+{
+    abstract class AbstractSpecialTransformation : IFunction
+    {
+        public object Evaluate(object value)
+        {
+            return value switch
+            {
+                null => EvaluateNull(),
+                DBNull _ => EvaluateNull(),
+                Null => EvaluateNull(),
+                Empty => EvaluateEmpty(),
+                Whitespace => EvaluateBlank(),
+                Any => EvaluateAny(),
+                Value => EvaluateValue(),
+                string s => EvaluateHighLevelString(s),
+                _ => EvaluateUncasted(value),
+            };
+        }
+
+        private object EvaluateUncasted(object value)
+        {
+            var caster = new TextCaster();
+            var str = caster.Execute(value);
+            return EvaluateHighLevelString(str);
+        }
+
+        protected virtual object EvaluateHighLevelString(string value)
+        {
+            if (new Empty().Equals(value))
+                return EvaluateEmpty();
+
+            if (new Null().Equals(value))
+                return EvaluateNull();
+
+            if (new Whitespace().Equals(value))
+                return EvaluateBlank();
+
+            if (new Any().Equals(value))
+                return EvaluateAny();
+
+            if (new Value().Equals(value))
+                return EvaluateValue();
+
+            return EvaluateString(value);
+        }
+
+        protected abstract object EvaluateNull();
+        protected abstract object EvaluateEmpty();
+        protected abstract object EvaluateBlank();
+        protected abstract object EvaluateAny();
+        protected abstract object EvaluateValue();
+        protected abstract object EvaluateString(string value);
+    }
+
+    class NullToValue : AbstractSpecialTransformation
+    {
+        protected override object EvaluateNull() => new Value().Keyword;
+        protected override object EvaluateEmpty() => new Empty().Keyword;
+        protected override object EvaluateBlank() => new Whitespace().Keyword;
+        protected override object EvaluateAny() => new Value().Keyword;
+        protected override object EvaluateValue() => new Value().Keyword;
+        protected override object EvaluateString(string value) => value;
+    }
+
+    class AnyToAny : AbstractSpecialTransformation
+    {
+        protected override object EvaluateNull() => new Any().Keyword;
+        protected override object EvaluateEmpty() => new Any().Keyword;
+        protected override object EvaluateBlank() => new Any().Keyword;
+        protected override object EvaluateAny() => new Any().Keyword;
+        protected override object EvaluateValue() => new Any().Keyword;
+        protected override object EvaluateString(string value) => new Any().Keyword;
+    }
+
+    class ValueToValue : AbstractSpecialTransformation
+    {
+        protected override object EvaluateNull() => new Null().Keyword;
+        protected override object EvaluateEmpty() => new Value().Keyword;
+        protected override object EvaluateBlank() => new Value().Keyword;
+        protected override object EvaluateAny() => new Value().Keyword;
+        protected override object EvaluateValue() => new Value().Keyword;
+        protected override object EvaluateString(string value) => new Value().Keyword;
+    }
+}
