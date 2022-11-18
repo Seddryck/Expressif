d728c553f7f74007b57a2cb3f37bbcfebeb4e992
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-11-18 19:01:25 +0100

    Parse predicate

---
+++ Expressif/Parsers/PredicateExpression.cs
@@ -0,0 +1,21 @@
+﻿using Sprache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Parsers
+{
+    public class PredicateExpression
+    {
+        public Function Member { get; }
+
+        public PredicateExpression(Function member)
+            => (Member) = (member);
+
+        public static readonly Parser<PredicateExpression> Parser =
+            from predicate in Function.Parser
+            select new PredicateExpression(predicate);
+    }
+}
---
+++ Expressif/Predicates/IPredicate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Functions
+{
+    public interface IPredicate : IFunction
+    {
+        new bool Evaluate(object? value);
+    }
+
+    public interface IPredicate<T> : IPredicate
+    {
+        bool Evaluate(T? value);
+    }
+}
