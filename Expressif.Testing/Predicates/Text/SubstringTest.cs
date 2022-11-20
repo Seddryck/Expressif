b5d9d37ddc792558a46b680ac8f7721cc8b9e525
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-11-20 10:37:11 +0100

    Add substring prediactes (startsWith, endsWith and contains)

---
+++ Expressif.Testing/Predicates/Text/SubstringTest.cs
@@ -0,0 +1,73 @@
+﻿using Expressif.Predicates.Text;
+using Expressif.Values.Resolvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Testing.Predicates.Text
+{
+    internal class SubstringTest
+    {
+        [TestCase("Foobar", "Foo", true)]
+        [TestCase("Foobar", "bar", false)]
+        [TestCase("Foobar", "oba", false)]
+        [TestCase("Foobar", "bing", false)]
+        [TestCase("Foobar", "FoobarBaz", false)]
+        [TestCase("", "(empty)", true)]
+        [TestCase("Foobar", "(empty)", true)]
+        [TestCase("(empty)", "", true)]
+        [TestCase("(empty)", "Foo", false)]
+        public void StartsWith_Text_Success(object value, object reference, bool expected)
+        {
+
+            var predicate = new StartsWith(new LiteralScalarResolver<string>(reference));
+            Assert.Multiple(() =>
+            {
+                Assert.That(predicate.Reference.Execute(), Is.EqualTo(reference));
+                Assert.That(predicate.Evaluate(value), Is.EqualTo(expected));
+            });
+        }
+
+        [TestCase("Foobar", "Foo", false)]
+        [TestCase("Foobar", "bar", true)]
+        [TestCase("Foobar", "oba", false)]
+        [TestCase("Foobar", "bing", false)]
+        [TestCase("Foobar", "FoobarBaz", false)]
+        [TestCase("", "(empty)", true)]
+        [TestCase("Foobar", "(empty)", true)]
+        [TestCase("(empty)", "", true)]
+        [TestCase("(empty)", "bar", false)]
+        public void EndsWith_Text_Success(object value, object reference, bool expected)
+        {
+
+            var predicate = new EndsWith(new LiteralScalarResolver<string>(reference));
+            Assert.Multiple(() =>
+            {
+                Assert.That(predicate.Reference.Execute(), Is.EqualTo(reference));
+                Assert.That(predicate.Evaluate(value), Is.EqualTo(expected));
+            });
+        }
+
+        [TestCase("Foobar", "Foo", true)]
+        [TestCase("Foobar", "bar", true)]
+        [TestCase("Foobar", "oba", true)]
+        [TestCase("Foobar", "bing", false)]
+        [TestCase("Foobar", "FoobarBaz", false)]
+        [TestCase("", "(empty)", true)]
+        [TestCase("Foobar", "(empty)", true)]
+        [TestCase("(empty)", "", true)]
+        [TestCase("(empty)", "bar", false)]
+        public void Contains_Text_Success(object value, object reference, bool expected)
+        {
+
+            var predicate = new Expressif.Predicates.Text.Contains(new LiteralScalarResolver<string>(reference));
+            Assert.Multiple(() =>
+            {
+                Assert.That(predicate.Reference.Execute(), Is.EqualTo(reference));
+                Assert.That(predicate.Evaluate(value), Is.EqualTo(expected));
+            });
+        }
+    }
+}
---
+++ Expressif/Predicates/Text/Substring.cs
@@ -0,0 +1,69 @@
+﻿using Expressif.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Predicates.Text
+{
+    abstract class BaseTextPredicateSubstring : BaseTextPredicateReference
+    {
+        protected StringComparison Comparison { get; }
+
+        public BaseTextPredicateSubstring(IScalarResolver<string> reference)
+            : this(reference, StringComparer.InvariantCultureIgnoreCase) { }
+
+        public BaseTextPredicateSubstring(IScalarResolver<string> reference, StringComparer comparer)
+            : base(reference) { Comparison = GetComparison(comparer); }
+
+        protected virtual StringComparison GetComparison(StringComparer comparer)
+        {
+            var map = new Dictionary<StringComparer, StringComparison>
+            {
+                { StringComparer.InvariantCulture, StringComparison.InvariantCulture },
+                { StringComparer.InvariantCultureIgnoreCase, StringComparison.InvariantCultureIgnoreCase },
+            };
+            if (map.TryGetValue(comparer, out var comparison))
+                return comparison;
+            else
+                throw new ArgumentException(nameof(comparer));
+        }
+    }
+
+    class StartsWith : BaseTextPredicateSubstring
+    {
+        public StartsWith(IScalarResolver<string> reference)
+            : base(reference, StringComparer.InvariantCultureIgnoreCase) { }
+
+        public StartsWith(IScalarResolver<string> reference, StringComparer comparer)
+            : base(reference,comparer) {}
+
+        protected override bool EvaluateText(string value, string reference)
+            => value.StartsWith(reference, Comparison);
+    }
+
+    class EndsWith : BaseTextPredicateSubstring
+    {
+        public EndsWith(IScalarResolver<string> reference)
+            : base(reference, StringComparer.InvariantCultureIgnoreCase) { }
+
+        public EndsWith(IScalarResolver<string> reference, StringComparer comparer)
+            : base(reference, comparer) { }
+
+        protected override bool EvaluateText(string value, string reference)
+            => value.EndsWith(reference, Comparison);
+    }
+
+    class Contains : BaseTextPredicateSubstring
+    {
+        public Contains(IScalarResolver<string> reference)
+            : base(reference, StringComparer.InvariantCultureIgnoreCase) { }
+
+        public Contains(IScalarResolver<string> reference, StringComparer comparer)
+            : base(reference, comparer) { }
+
+        protected override bool EvaluateText(string value, string reference)
+            => value.Contains(reference, Comparison);
+    }
+}
