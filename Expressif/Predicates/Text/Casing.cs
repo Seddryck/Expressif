40b45764d597388261a3fa14dda4a72a897f264e
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-11-20 10:46:59 +0100

    Casing text predicates (lowerCase and upperCase)

---
+++ Expressif/Predicates/Text/Casing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Predicates.Text
+{
+    class LowerCase : BaseTextPredicateWithoutReference
+    {
+        protected override bool EvaluateNull() => true;
+        protected override bool EvaluateText(string value)
+            => value.Equals(value.ToLowerInvariant(), StringComparison.InvariantCulture);
+    }
+
+    class UpperCase : BaseTextPredicateWithoutReference
+    {
+        protected override bool EvaluateNull() => true;
+        protected override bool EvaluateText(string value)
+            => value.Equals(value.ToUpperInvariant(), StringComparison.InvariantCulture);
+    }
+}
