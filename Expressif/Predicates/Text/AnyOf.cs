c064494e4a219dd5c510d69bf14959a16c2e0bdf
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-11-20 14:01:23 +0100

    Implement AnyOf text predicate

---
+++ Expressif/Predicates/Text/AnyOf.cs
@@ -0,0 +1,32 @@
+﻿using Expressif.Values;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Predicates.Text
+{
+    class AnyOf : BaseTextPredicate
+    {
+        public IEnumerable<IScalarResolver<string>> References { get; }
+        protected StringComparer Comparer { get; }
+
+        public AnyOf(IEnumerable<IScalarResolver<string>> references)
+            : this(references, StringComparer.InvariantCultureIgnoreCase) { }
+        public AnyOf(IEnumerable<IScalarResolver<string>> references, StringComparer comparer)
+                   => (References, Comparer) = (references, comparer);
+
+        protected override bool EvaluateBaseText(string value)
+        {
+            foreach (var reference in References)
+            {
+                var predicate = new EquivalentTo(reference, Comparer);
+                if (predicate.Evaluate(value))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
