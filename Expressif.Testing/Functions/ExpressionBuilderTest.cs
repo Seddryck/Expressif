aaf5c78a5ddeca9ab134e2c3abc997a2c67aaa2c
Cédric L. Charlier <seddryck@gmail.com>
2022-12-11 11:27:21 +0100

    First attempt

---
+++ Expressif.Testing/Functions/ExpressionBuilderTest.cs
@@ -0,0 +1,72 @@
+﻿using Expressif.Functions;
+using Expressif.Functions.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Testing.Functions
+{
+    public class ExpressionBuilderTest
+    {
+        [Test]
+        public void As_WithoutParameter_CorrectlyEvaluate()
+        {
+            var builder = new ExpressionBuilder();
+            builder.As<Lower>();
+            var expression = builder.Build();
+            Assert.That(expression.Evaluate("Nikola Tesla"), Is.EqualTo("nikola tesla"));
+        }
+
+        [Test]
+        public void As_WithParameter_CorrectlyEvaluate()
+        {
+            var builder = new ExpressionBuilder();
+            builder.As<FirstChars>(5);
+            var expression = builder.Build();
+            Assert.That(expression.Evaluate("Nikola Tesla"), Is.EqualTo("Nikol"));
+        }
+
+        [Test]
+        public void As_WithParameters_CorrectlyEvaluate()
+        {
+            var builder = new ExpressionBuilder();
+            builder.As<PadRight>(15, '*');
+            var expression = builder.Build();
+            Assert.That(expression.Evaluate("Nikola Tesla"), Is.EqualTo("Nikola Tesla***"));
+        }
+
+        [Test]
+        public void Chain_WithParameters_CorrectlyEvaluate()
+        {
+            var builder = new ExpressionBuilder();
+            builder.As<Lower>().Chain<FirstChars>(5).Chain<PadRight>(7, '*');
+            var expression = builder.Build();
+            Assert.That(expression.Evaluate("Nikola Tesla"), Is.EqualTo("nikol**"));
+        }
+
+        [Test]
+        public void Chain_NotGenericWithParameters_CorrectlyEvaluate()
+        {
+            var builder = new ExpressionBuilder();
+            builder.As(typeof(Lower)).Chain(typeof(FirstChars), 5).Chain(typeof(PadRight), 7, '*');
+            var expression = builder.Build();
+            Assert.That(expression.Evaluate("Nikola Tesla"), Is.EqualTo("nikol**"));
+        }
+
+        [Test]
+        public void Chain_SubExpression_CorrectlyEvaluate()
+        {
+            var subbuilder = new ExpressionBuilder();
+            subbuilder.As<FirstChars>(5).Chain<PadRight>(7, '*');
+            
+            var builder = new ExpressionBuilder();
+            builder.As<Lower>().Chain(subbuilder).Chain<Upper>();
+
+            var expression = builder.Build();
+            Assert.That(expression.Evaluate("Nikola Tesla"), Is.EqualTo("NIKOL**"));
+        }
+    }   
+}
+    
---
+++ Expressif/Functions/ExpressionBuilder.cs
@@ -0,0 +1,71 @@
+﻿using Expressif.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Functions
+{
+    public class ExpressionBuilder
+    {
+        private Context Context { get; }
+        private ExpressionFactory Factory { get; }
+        public ExpressionBuilder()
+            : this(new Context()) { }
+        public ExpressionBuilder(Context context)
+            : this(context, new ExpressionFactory()) { }
+        public ExpressionBuilder(Context context, ExpressionFactory factory)
+            => (Context, Factory) = (context, factory);
+
+        public ExpressionMember? Root { get; private set; }
+
+        public ExpressionMember As<T>(params object[] parameters) where T : IFunction
+            => As(typeof(T), parameters);
+
+        public ExpressionMember As(Type type, params object[] parameters)
+            => Root = new ExpressionMember(type, parameters, Context, Factory);
+
+        public IFunction Build()
+        {
+            if (Root is null)
+                throw new InvalidOperationException();
+            
+            IFunction function = Root.Value;    
+            var member = Root;
+            while (member.Next is not null)
+            {
+                member = member.Next;
+                function = new ChainFunction(new[] { function, member.Value });
+            }
+            return function;
+        }
+
+        public class ExpressionMember
+        {
+            private Context Context { get; }
+            private ExpressionFactory Factory { get; }
+            public IFunction Value { get; private set; }
+            public ExpressionMember(Type type, object[] parameters, Context context, ExpressionFactory factory)
+            {
+                (Context, Factory) = (context, factory);
+                var typedParameters = new List<IParameter>();
+                foreach (var parameter in parameters)
+                {
+                    if (parameter is not IParameter)
+                        typedParameters.Add(new LiteralParameter(parameter.ToString()!));
+                }
+                Value = factory.Instantiate(type, typedParameters.ToArray(), context);
+            }
+
+            public ExpressionMember? Next { get; private set; }
+
+            public ExpressionMember Chain<T>(params object[] parameters) where T : IFunction
+                => Chain(typeof(T), parameters);
+
+            public ExpressionMember Chain(Type type, params object[] parameters)
+                => Next = new ExpressionMember(type, parameters, Context, Factory);
+
+        }
+    }
+}
