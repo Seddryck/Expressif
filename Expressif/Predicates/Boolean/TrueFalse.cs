56453aaad3c97708a340c912178b607245b59d57
Cédric L. Charlier <seddryck@gmail.com>
2022-12-21 18:39:21 +0100

    Document boolean predicates

--- Expressif/Predicates/Boolean/TrueFalse.cs
+++ Expressif/Predicates/Boolean/TrueFalse.cs
@@ -7,21 +7,33 @@ using System.Threading.Tasks;
 
 namespace Expressif.Predicates.Boolean
 {
+    /// <summary>
+    /// Returns `true` if the argument is effectively `true` else return `false`.
+    /// </summary>
     class True : BaseBooleanPredicate
     {
         protected override bool EvaluateBoolean(bool boolean) => boolean;
     }
 
+    /// <summary>
+    /// Returns `true` if the argument is effectively `true` or `null` else return `false`.
+    /// </summary>
     class TrueOrNull : True
     {
         protected override bool EvaluateNull() => true;
     }
 
+    /// <summary>
+    /// Returns `true` if the argument is effectively `false` else return `false`.
+    /// </summary>
     class False : BaseBooleanPredicate
     {
         protected override bool EvaluateBoolean(bool boolean) => !boolean;
     }
 
+    /// <summary>
+    /// Returns `true` if the argument is effectively `false` or `null` else return `false`.
+    /// </summary>
     class FalseOrNull : False
     {
         protected override bool EvaluateNull() => true;
