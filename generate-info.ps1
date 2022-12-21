cdcdd903719977d3499bef3ba452bdc875289298
Cédric L. Charlier <seddryck@gmail.com>
2022-12-21 20:38:18 +0100

    Review script to create reference .json to be more generic

--- generate-info.ps1
+++ generate-info.ps1
@@ -1,7 +1,10 @@
 ﻿#requires -PSEdition Core
+param (
+    [string] $class
+)
 
 $destinationPath = ".\docs\_data"
-$destinationFile = "function.json"
+$destinationFile = "$($class.ToLower()).json"
 
 
 ########### Check if it's useful to make changes to doc or readme #############
@@ -12,7 +15,6 @@ If(Test-Path -LiteralPath $destinationPath\$destinationFile -PathType leaf) {
     $hash = Get-FileHash $destinationPath\$destinationFile
     Write-Debug "Previous hash for $destinationPath\$destinationFile is $($hash.Hash)"
 }
-
 
 ########### Generate JSON file #############
 
@@ -29,11 +31,12 @@ Add-Type -Path "$directory\$dllfile"
 
 Set-Location "..\..\"
 
-Write-Host "Generating JSON for functions based on $assemblyPath\$directory\$dllfile"
+Write-Host "Generating JSON for $($class.ToLower())s based on $assemblyPath\$directory\$dllfile"
 $elapsed = Measure-Command -Expression {
-    $locator = New-Object  Expressif.Functions.Introspection.FunctionIntrospector
+    $TextInfo = (Get-Culture).TextInfo
+    $locator = New-Object -TypeName "Expressif.$($TextInfo.ToTitleCase($class))s.Introspection.$($TextInfo.ToTitleCase($class))Introspector"
     $functions = $locator.Locate() | Sort-Object ListingPriority | Select-Object -Property Name, Aliases, Scope, Summary, Parameters
-    Write-Host  "`t$($functions.Count) functions identified"
+    Write-Host  "`t$($functions.Count) $($class.ToLower()) identified"
     $functions | ForEach-Object {Write-Host "`t`t$($_.Name)"}
     $functions | ConvertTo-Json -depth 4 | Out-File "$destinationPath\$destinationFile"
 }
@@ -43,9 +46,9 @@ Write-Host  "File created at $destinationPath\$destinationFile in $($elapsed.Tot
 ########### Check if it's useful to report a change #############
 
 If ($hash.Hash -eq (Get-FileHash $destinationPath\$destinationFile).Hash) {
-    Write-Host "No change detected in the list of functions."
+    Write-Host "No change detected in the list of predicates."
     Exit 0
 } else {
-    Write-Host "Changes detected in the list of functions."
+    Write-Host "Changes detected in the list of predicates."
     Exit 1
 }
