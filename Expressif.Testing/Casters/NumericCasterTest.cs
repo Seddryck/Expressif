a666c0cde10cffb655f60771cc399e1b065fb6d9
Cédric L. Charlier <seddryck@gmail.com>
2022-12-24 13:22:45 +0100

    Increase code coverage

--- Expressif.Testing/Casters/NumericCasterTest.cs
+++ Expressif.Testing/Casters/NumericCasterTest.cs
@@ -26,5 +26,38 @@ namespace Expressif.Testing.Casters
            => Assert.That(new NumericCaster().Execute(
                 Activator.CreateInstance(type)!)
                 , Is.EqualTo(0));
+
+        [Test]
+        [TestCase(typeof(Byte))]
+        [TestCase(typeof(SByte))]
+        [TestCase(typeof(Int16))]
+        [TestCase(typeof(Int32))]
+        [TestCase(typeof(Int64))]
+#if NET7_0_OR_GREATER
+        [TestCase(typeof(Int128))]
+#endif
+        [TestCase(typeof(UInt16))]
+        [TestCase(typeof(UInt32))]
+        [TestCase(typeof(UInt64))]
+#if NET7_0_OR_GREATER
+        [TestCase(typeof(UInt128))]
+        [TestCase(typeof(Half))]
+#endif
+        [TestCase(typeof(Single))]
+        [TestCase(typeof(Double))]
+        [TestCase(typeof(Decimal))]
+        public void IsNumericType_Type_Valid(Type type)
+           => Assert.That(new NumericCaster().IsNumericType(Activator.CreateInstance(type)!), Is.True);
+
+        [Test]
+        [TestCase(typeof(string))]
+        [TestCase(typeof(bool))]
+        [TestCase(typeof(DateTime))]
+        [TestCase(typeof(DateOnly))]
+        [TestCase(typeof(TimeOnly))]
+        [TestCase(typeof(DateTimeOffset))]
+        [TestCase(typeof(TimeSpan))]
+        public void IsNumericType_Type_Invalid(Type type)
+           => Assert.That(new NumericCaster().IsNumericType(type), Is.False);
     }
 }
