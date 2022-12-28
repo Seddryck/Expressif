30bc7ddbfe963cfd9a504df82faa3797f743c07f
Cédric L. Charlier <seddryck@gmail.com>
2022-12-28 13:00:01 +0100

    New functions Sign and Absolute for numerics

---
+++ Expressif.Testing/Functions/Numeric/SignFunctionsTest.cs
@@ -0,0 +1,32 @@
+﻿using Expressif.Functions.Numeric;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Testing.Functions.Numeric
+{
+    public class SignFunctionsTest
+    {
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(-1, -1)]
+        [TestCase(5, 1)]
+        [TestCase(-5, -1)]
+        [TestCase(null, null)]
+        public void Sign_Valid(object value, decimal? expected)
+            => Assert.That(new Sign().Evaluate(value), Is.EqualTo(expected));
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(-1, 1)]
+        [TestCase(5, 5)]
+        [TestCase(-5, 5)]
+        [TestCase(null, null)]
+        public void Absolute_Valid(object value, decimal? expected)
+            => Assert.That(new Absolute().Evaluate(value), Is.EqualTo(expected));
+    }
+}
