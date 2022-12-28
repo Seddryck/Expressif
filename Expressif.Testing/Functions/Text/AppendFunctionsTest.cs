9f8d3681ab50b2f6bc81a7b000a52719a4175627
Cédric L. Charlier <seddryck@gmail.com>
2022-12-28 16:23:25 +0100

    Replace-slice function

---
+++ Expressif.Testing/Functions/Text/AppendFunctionsTest.cs
@@ -0,0 +1,48 @@
+﻿using Expressif.Functions.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Testing.Functions.Text
+{
+    public class AppendFunctionsTest
+    {
+
+        [Test]
+        [TestCase("123456789", "abc", "abc123456789")]
+        [TestCase("(null)", "abc", "(null)")]
+        [TestCase("(empty)", "abc", "abc")]
+        [TestCase("(blank)", "abc", "abc")]
+        public void Prefix_Valid(string value, string prefix, string expected)
+            => Assert.That(new Prefix(() => prefix).Evaluate(value)
+                , Is.EqualTo(expected));
+
+        [Test]
+        [TestCase("123456789", "abc", "123456789abc")]
+        [TestCase("(null)", "abc", "(null)")]
+        [TestCase("(empty)", "abc", "abc")]
+        [TestCase("(blank)", "abc", "abc")]
+        public void Suffix_Valid(string value, string suffix, string expected)
+            => Assert.That(new Suffix(() => suffix).Evaluate(value)
+                , Is.EqualTo(expected));
+
+
+        [Test]
+        [TestCase("123456789", 3, 2, "abc", "123abc6789")]
+        [TestCase("123456789", 3, 5, "abc", "123abc9")]
+        [TestCase("123456789", 3, 20, "abc", "123abc")]
+        [TestCase("123456789", -3, 5, "abc", "abc3456789")]
+        [TestCase("123456789", -3, 2, "abc", "abc123456789")]
+        [TestCase("123456789", 8, -2, "abc", "123456abc9")]
+        [TestCase("123456789", 20, -2, "abc", "123456789abc")]
+        [TestCase("123456789", 5, -20, "abc", "abc6789")]
+        [TestCase("(null)", 3, 2, "abc", "(null)")]
+        [TestCase("(empty)", 3, 2, "abc", "abc")]
+        [TestCase("(blank)", 3, 2, "abc", "abc")]
+        public void ReplaceSlice_Valid(string value, int start, int length, string append, string expected)
+            => Assert.That(new ReplaceSlice(() => start, () => length, () => append).Evaluate(value)
+                , Is.EqualTo(expected));
+    }
+}
--- Expressif/Functions/Text/AppendFunctions.cs
+++ Expressif/Functions/Text/AppendFunctions.cs
@@ -39,4 +39,45 @@ namespace Expressif.Functions.Text
         protected override object EvaluateString(string value) => $"{value}{Append.Invoke()}";
     }
 
+
+    /// <summary>
+    /// Returns the argument value with a subset of the string substitued by a another string.
+    /// </summary>
+    public class ReplaceSlice : BaseTextAppend
+    {
+        public Func<int> Start { get; }
+        public Func<int> Length { get; }
+
+        /// <param name="start">The position to start to replace</param>
+        /// <param name="length">The length to replace</param>
+        /// <param name="append">The text to append when the slice has been removed</param>
+        public ReplaceSlice(Func<int> start, Func<int> length, Func<string> append)
+            : base(append) { (Start, Length) = (start, length); }
+        protected override object EvaluateString(string value)
+        {
+            var start = Start.Invoke();
+            var length = Length.Invoke();
+
+            if (length<0)
+            {
+                start += length;
+                length = Math.Abs(length);
+            }
+
+            if (start>= value.Length)
+                return $"{value}{Append.Invoke()}";
+            if (start + length <=0)
+                return $"{Append.Invoke()}{value}";
+
+            var text = new StringBuilder();
+            if (start >= 0)
+                text.Append(value[..start]);
+            text.Append(Append.Invoke());
+            if (start+length<=value.Length)
+                text.Append(value[(start + length) ..]);
+
+            return text.ToString();
+        }
+    }
+
 }
