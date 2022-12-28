dd91058bdf472b422fc0df372ff6a1dfe2c142d5
Cédric L. Charlier <seddryck@gmail.com>
2022-12-28 12:50:10 +0100

    New predicates to shortcut some common comparisons

---
+++ Expressif/Predicates/Numeric/ComparisonShortcut.cs
@@ -0,0 +1,66 @@
+﻿using Expressif.Values;
+using Expressif.Values.Casters;
+using Expressif.Values.Special;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Predicates.Numeric
+{
+    /// <summary>
+    /// Returns true if the numeric argument is equal to 0.
+    /// </summary>
+    public class Zero : EqualTo
+    {
+        public Zero()
+            : base(() => 0) { }
+    }
+
+    /// <summary>
+    /// Returns true if the numeric argument is equal to 1.
+    /// </summary>
+    public class One : EqualTo
+    {
+        public One()
+            : base(() => 1) { }
+    }
+
+    /// <summary>
+    /// Returns true if the numeric argument is greater than 0.
+    /// </summary>
+    public class Positive : GreaterThan
+    {
+        public Positive()
+            : base(() => 0) { }
+    }
+
+    /// <summary>
+    /// Returns true if the numeric argument is greater or equal to 0.
+    /// </summary>
+    public class PositiveOrZero : GreaterThanOrEqual
+    {
+        public PositiveOrZero()
+            : base(() => 0) { }
+    }
+
+    /// <summary>
+    /// Returns true if the numeric argument is less than 0.
+    /// </summary>
+    public class Negative : LessThan
+    {
+        public Negative()
+            : base(() => 0) { }
+    }
+
+
+    /// <summary>
+    /// Returns true if the numeric argument is less or equal to 0.
+    /// </summary>
+    public class NegativeOrZero : LessThanOrEqual
+    {
+        public NegativeOrZero()
+            : base(() => 0) { }
+    }
+}
