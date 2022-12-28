2558fee38bf2e4274da53b00e7c32d34499bc01c
Cédric L. Charlier <seddryck@gmail.com>
2022-12-28 15:09:08 +0100

    PadCenter function

---
+++ Expressif.Testing/Functions/Text/PaddingFunctionsTest.cs
@@ -0,0 +1,49 @@
+﻿using Expressif.Functions.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Testing.Functions.Text
+{
+    public class PaddingFunctionsTest
+    {
+        [Test]
+        [TestCase("1234", 9, '0', "123400000")]
+        [TestCase(1234, 9, '0', "123400000")]
+        [TestCase("1234", 9, '*', "1234*****")]
+        [TestCase(1234, 9, '*', "1234*****")]
+        [TestCase("123456789", 3, '0', "123456789")]
+        [TestCase("(null)", 3, '0', "000")]
+        [TestCase("(empty)", 3, '0', "000")]
+        public void PadRight_Valid(object value, int length, char character, string expected)
+            => Assert.That(new PadRight(() => (length), () => (character))
+                .Evaluate(value), Is.EqualTo(expected));
+
+        [Test]
+        [TestCase("1234", 9, '0', "000001234")]
+        [TestCase("1234", 9, '*', "*****1234")]
+        [TestCase("123456789", 3, '0', "123456789")]
+        [TestCase("(null)", 3, '0', "000")]
+        [TestCase("(empty)", 3, '0', "000")]
+        public void PadLeft_Valid(string value, int length, char character, string expected)
+            => Assert.That(new PadLeft(() => (length), () => (character))
+                .Evaluate(value), Is.EqualTo(expected));
+
+
+        [Test]
+        [TestCase("1234", 4, '*', "1234")]
+        [TestCase("1234", 5, '*', "1234*")]
+        [TestCase("1234", 6, '*', "*1234*")]
+        [TestCase("1234", 7, '*', "*1234**")]
+        [TestCase("1234", 8, '*', "**1234**")]
+        [TestCase("1234", 9, '*', "**1234***")]
+        [TestCase("1234", 2, '*', "1234")]
+        [TestCase("(null)", 3, '0', "000")]
+        [TestCase("(empty)", 3, '0', "000")]
+        public void PadCenter_Valid(string value, int length, char character, string expected)
+            => Assert.That(new PadCenter(() => (length), () => (character))
+                .Evaluate(value), Is.EqualTo(expected));
+    }
+}
--- Expressif/Functions/Text/PaddingFunctions.cs
+++ Expressif/Functions/Text/PaddingFunctions.cs
@@ -48,4 +48,29 @@ namespace Expressif.Functions.Text
             => value.Length >= Length.Invoke() ? value : value.PadLeft(Length.Invoke(), Character.Invoke());
     }
 
+    /// <summary>
+    /// Returns a new string that center-aligns the characters in this string by padding them on both the left and the right with a specified character, for a specified total length. If the padding cannot be symetrical then the padding char is added on the right. If the length of the argument value is longer than the parameter value then the argument value is returned unmodified. 
+    /// </summary>
+    public class PadCenter : BasePaddingFunction
+    {
+        /// <param name="length">An integer value between 0 and +Infinity, defining the minimal length of the string returned</param>
+        /// <param name="character">The padding character</param>
+        public PadCenter(Func<int> length, Func<char> character)
+            : base(length, character) { }
+
+        protected override object EvaluateString(string value)
+        {
+            var length = Length.Invoke();
+            if (value.Length >= length)
+                return value;
+
+            var left = Convert.ToInt16(Math.Floor((length - value.Length) / 2.0));
+            var right = Convert.ToInt16(Math.Ceiling((length - value.Length) / 2.0));
+            var character = Character.Invoke();
+
+            var text = new StringBuilder().Append(character, left).Append(value).Append(character, right);
+            return text.ToString();
+        }
+    }
+
 }
