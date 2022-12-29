dd4c62f75f693c2ce95cb60c91834372fd2b4a62
Cédric L. Charlier <seddryck@gmail.com>
2022-12-29 15:29:05 +0100

    Tomorrow, today and yesterday predicates

---
+++ Expressif.Testing/Predicates/Temporal/ComparisonAroundTodayPredicatesTest.cs
@@ -0,0 +1,59 @@
+﻿using Expressif.Predicates.Temporal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Expressif.Testing.Predicates.Temporal
+{
+    public class ComparisonAroundTodayPredicatesTest
+    {
+        public ComparisonAroundTodayPredicatesTest()
+        {
+            TypeDescriptor.AddAttributes(
+                typeof(DateOnly)
+                , new TypeConverterAttribute(typeof(Expressif.Values.Converters.DateOnlyConverter))
+            );
+        }
+
+        [Test]
+        [TestCase("2022-12-28", false)]
+        [TestCase("2022-12-29", false)]
+        [TestCase("2022-12-30", true)]
+        [TestCase("1970-05-06", false)]
+        public void Tomorrow_Date_Valid(string text, bool expected)
+            => Assert.That(new Tomorrow(new DateTime(2022, 12, 29))
+                    .Evaluate(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(text)!
+                    )
+                , Is.EqualTo(expected));
+
+        [Test]
+        [TestCase("2022-12-28", false)]
+        [TestCase("2022-12-29", false)]
+        [TestCase("2022-12-30", true)]
+        [TestCase("1970-05-06", false)]
+        public void Tomorrow_DateTime_Valid(DateTime dt, bool expected)
+            => Assert.That(new Tomorrow(new DateTime(2022,12,29)).Evaluate(dt), Is.EqualTo(expected));
+
+        [Test]
+        [TestCase("2022-12-28", false)]
+        [TestCase("2022-12-29", true)]
+        [TestCase("2022-12-30", false)]
+        [TestCase("1970-05-06", false)]
+        public void Today_DateTime_Valid(DateTime dt, bool expected)
+            => Assert.That(new Today(new DateTime(2022, 12, 29)).Evaluate(dt), Is.EqualTo(expected));
+
+        [Test]
+        [TestCase("2022-12-28", true)]
+        [TestCase("2022-12-29", false)]
+        [TestCase("2022-12-30", false)]
+        [TestCase("1970-05-06", false)]
+        public void Yesterday_DateTime_Valid(DateTime dt, bool expected)
+            => Assert.That(new Yesterday(new DateTime(2022, 12, 29)).Evaluate(dt), Is.EqualTo(expected));
+    }
+}
---
+++ Expressif/Predicates/Temporal/ComparisonTodayPredicates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Predicates.Temporal
+{
+    public abstract class BaseTemporalAroundNowPredicate : BaseDateTimePredicate
+    {
+        protected DateOnly Today { get; }
+        public BaseTemporalAroundNowPredicate()
+            : this(DateTime.Now) { }
+
+        protected BaseTemporalAroundNowPredicate(DateOnly today)
+            => Today = today;
+
+        protected BaseTemporalAroundNowPredicate(DateTime now)
+            : this(DateOnly.FromDateTime(now)) { }
+
+        protected override bool EvaluateDateTime(DateTime dt)
+            => EvaluateDate(DateOnly.FromDateTime(dt));
+    }
+
+    /// <summary>
+    /// Returns true if the date passed as argument is representing the next date compared to the current date. Returns false otherwise.
+    /// </summary>
+    public class Tomorrow : BaseTemporalAroundNowPredicate
+    {
+        internal Tomorrow(DateTime now)
+            : base(now) { }
+
+        protected override bool EvaluateDate(DateOnly date)
+            => date == Today.AddDays(1);
+    }
+
+    /// <summary>
+    /// Returns true if the date passed as argument is representing the current date. Returns false otherwise.
+    /// </summary>
+    public class Today : BaseTemporalAroundNowPredicate
+    {
+        internal Today(DateTime now)
+            : base(now) { }
+
+        protected override bool EvaluateDate(DateOnly date)
+            => date == Today;
+    }
+
+    /// <summary>
+    /// Returns true if the date passed as argument is representing the previous date compared to the current date. Returns false otherwise.
+    /// </summary>
+    public class Yesterday : BaseTemporalAroundNowPredicate
+    {
+        internal Yesterday(DateTime now)
+            : base(now) { }
+
+        protected override bool EvaluateDate(DateOnly date)
+            => date == Today.AddDays(-1);
+    }
+}
+
