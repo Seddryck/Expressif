7e78f6063e9046280d83e297fc265821d03fcaaa
Cédric L. Charlier <seddryck@gmail.com>
2022-12-30 19:03:04 +0100

    Next and previous weekday

---
+++ Expressif.Testing/Functions/Temporal/WeekdayFunctionsTest.cs
@@ -0,0 +1,97 @@
+﻿using Expressif.Functions.Temporal;
+using Expressif.Predicates.Temporal;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Testing.Functions.Temporal
+{
+    public class WeekdayFunctionsTest
+    {
+        public WeekdayFunctionsTest()
+        {
+            TypeDescriptor.AddAttributes(
+                typeof(DateOnly)
+                , new TypeConverterAttribute(typeof(Expressif.Values.Converters.DateOnlyConverter))
+            );
+        }
+
+        [Test]
+        [TestCase("2022-12-28", "Tuesday", "2023-01-03")]
+        [TestCase("2022-12-28", "Wednesday", "2023-01-04")]
+        [TestCase("2022-12-28", "Thursday", "2022-12-29")]
+        public void NextWeekday_Date_Valid(string argument, string dayOfWeek, string expected)
+            => Assert.That(new NextWeekday(() => (Expressif.Values.Weekday)
+                        TypeDescriptor.GetConverter(typeof(Expressif.Values.Weekday))
+                        .ConvertFromInvariantString(dayOfWeek)!
+                    )
+                    .Evaluate(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(argument)!
+                    )
+                , Is.EqualTo(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(expected)!
+                ));
+
+
+        [Test]
+        [TestCase("2022-12-28", "Tuesday", "2023-01-03")]
+        [TestCase("2022-12-28", "Wednesday", "2022-12-28")]
+        [TestCase("2022-12-28", "Thursday", "2022-12-29")]
+        public void NextWeekdayOrSame_Date_Valid(string argument, string dayOfWeek, string expected)
+            => Assert.That(new NextWeekdayOrSame(() => (Expressif.Values.Weekday)
+                        TypeDescriptor.GetConverter(typeof(Expressif.Values.Weekday))
+                        .ConvertFromInvariantString(dayOfWeek)!
+                    )
+                    .Evaluate(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(argument)!
+                    )
+                , Is.EqualTo(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(expected)!
+                ));
+
+
+        [Test]
+        [TestCase("2022-12-28", "Tuesday", "2022-12-27")]
+        [TestCase("2022-12-28", "Wednesday", "2022-12-21")]
+        [TestCase("2022-12-28", "Thursday", "2022-12-22")]
+        public void PreviousWeekday_Date_Valid(string argument, string dayOfWeek, string expected)
+            => Assert.That(new PreviousWeekday(() => (Expressif.Values.Weekday)
+                        TypeDescriptor.GetConverter(typeof(Expressif.Values.Weekday))
+                        .ConvertFromInvariantString(dayOfWeek)!
+                    )
+                    .Evaluate(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(argument)!
+                    )
+                , Is.EqualTo(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(expected)!
+                ));
+
+        [Test]
+        [TestCase("2022-12-28", "Tuesday", "2022-12-27")]
+        [TestCase("2022-12-28", "Wednesday", "2022-12-28")]
+        [TestCase("2022-12-28", "Thursday", "2022-12-22")]
+        public void PreviousWeekdayOrSame_Date_Valid(string argument, string dayOfWeek, string expected)
+            => Assert.That(new PreviousWeekdayOrSame(() => (Expressif.Values.Weekday)
+                        TypeDescriptor.GetConverter(typeof(Expressif.Values.Weekday))
+                        .ConvertFromInvariantString(dayOfWeek)!
+                    )
+                    .Evaluate(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(argument)!
+                    )
+                , Is.EqualTo(
+                        TypeDescriptor.GetConverter(typeof(DateOnly))
+                        .ConvertFromInvariantString(expected)!
+                ));
+    }
+}
---
+++ Expressif/Functions/Temporal/WeekdayFunctions.cs
@@ -0,0 +1,89 @@
+﻿using Expressif.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Functions.Temporal
+{
+    public abstract class BaseTemporalWeekdayFunction : BaseTemporalFunction
+    {
+        protected override object EvaluateDateTime(DateTime dt)
+            => EvaluateDate(DateOnly.FromDateTime(dt));
+
+        protected abstract object EvaluateDate(DateOnly date);
+    }
+
+    /// <summary>
+    /// Returns a new date value corresponding to the occurrence of the weekday, passed as a parameter, following the date passed as the argument.
+    /// </summary>
+    public class NextWeekday : BaseTemporalWeekdayFunction
+    {
+        public Func<Weekday> DayOfWeek { get; }
+
+        /// <param name="weekday">The day of week to compare to the argument</param>
+        public NextWeekday(Func<Weekday> weekday)
+            : base() { DayOfWeek = weekday; }
+
+        protected override object EvaluateDate(DateOnly date)
+        {
+            var forward = DayOfWeek.Invoke().Index - date.ToWeekday().Index;
+            return date.AddDays(forward <= 0 ? forward + 7 : forward);
+        }
+    }
+
+    /// <summary>
+    /// Returns a new date value corresponding to the occurrence of the weekday passed as a parameter following the date passed as the argument except if this date corresponds to the expected weekday then it returns this date.
+    /// </summary>
+    public class NextWeekdayOrSame : BaseTemporalWeekdayFunction
+    {
+        public Func<Weekday> DayOfWeek { get; }
+
+        /// <param name="weekday">The day of week to compare to the argument</param>
+        public NextWeekdayOrSame(Func<Weekday> weekday)
+            : base() { DayOfWeek = weekday; }
+
+        protected override object EvaluateDate(DateOnly date)
+        {
+            var forward = DayOfWeek.Invoke().Index - date.ToWeekday().Index;
+            return date.AddDays(forward < 0 ? forward + 7 : forward);
+        }
+    }
+
+    /// <summary>
+    /// Returns a new dateTime value corresponding to the occurrence of the weekday passed as a parameter preceding the date passed as the argument.
+    /// </summary>
+    public class PreviousWeekday : BaseTemporalWeekdayFunction
+    {
+        public Func<Weekday> DayOfWeek { get; }
+
+        /// <param name="weekday">The day of week to compare to the argument</param>
+        public PreviousWeekday(Func<Weekday> weekday)
+            : base() { DayOfWeek = weekday; }
+
+        protected override object EvaluateDate(DateOnly date)
+        {
+            var backward = DayOfWeek.Invoke().Index - date.ToWeekday().Index;
+            return date.AddDays(backward >= 0 ? backward - 7 : backward);
+        }
+    }
+
+    /// <summary>
+    /// Returns a new dateTime value corresponding to the occurrence of the weekday passed as a parameter preceding the date passed as the argument except if this date corresponds to the expected weekday then it returns this date.
+    /// </summary>
+    public class PreviousWeekdayOrSame : BaseTemporalWeekdayFunction
+    {
+        public Func<Weekday> DayOfWeek { get; }
+
+        /// <param name="weekday">The day of week to compare to the argument</param>
+        public PreviousWeekdayOrSame(Func<Weekday> weekday)
+            : base() { DayOfWeek = weekday; }
+
+        protected override object EvaluateDate(DateOnly date)
+        {
+            var backward = DayOfWeek.Invoke().Index - date.ToWeekday().Index;
+            return date.AddDays(backward > 0 ? backward - 7 : backward);
+        }
+    }
+}
