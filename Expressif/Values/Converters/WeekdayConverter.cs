11d3a68d7a69486e1222841c0cebdaf3e97ae95c
Cédric L. Charlier <seddryck@gmail.com>
2022-12-30 17:03:04 +0100

    New record Weekday to handle a day of the week based on its English name

---
+++ Expressif/Values/Converters/WeekdayConverter.cs
@@ -0,0 +1,51 @@
+﻿using Expressif.Values.Casters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Values.Converters
+{
+
+    public class WeekdayConverter : TypeConverter
+    {
+        private IParser<Weekday> Parser { get; }
+
+        public WeekdayConverter()
+            : this(new WeekdayCaster()) { }
+
+        public WeekdayConverter(IParser<Weekday> parser)
+            => Parser = parser;
+
+        public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+            => sourceType switch
+            {
+                Type _ when sourceType == typeof(string) => true,
+                _ => base.CanConvertFrom(context, sourceType),
+            };
+
+        public override object ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+        {
+            if (value is string str)
+                return Parser.Parse(str);
+            return base.ConvertFrom(context, culture, value) ?? throw new NotSupportedException();
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext? context, Type? sourceType)
+            => sourceType switch
+            {
+                Type _ when sourceType == typeof(string) => true,
+                _ => base.CanConvertTo(context, sourceType),
+            };
+
+        public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
+        {
+            if (CanConvertTo(destinationType)&& value is Weekday weekday)
+                    return weekday.ToString();
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
---
+++ Expressif/Values/Weekday.cs
@@ -0,0 +1,61 @@
+﻿using Expressif.Values.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressif.Values
+{
+    [TypeConverter(typeof(WeekdayConverter))]
+    public record Weekday(int Index, string Name)
+    { }
+
+    public static class Weekdays
+    {
+        internal static Weekday GetByIndex(DayOfWeek dayOfWeek)
+            => TryGetByIndex(((int)dayOfWeek + 6) % 7, out var value) ? value : throw new ArgumentOutOfRangeException(nameof(dayOfWeek));
+        internal static bool TryGetByIndex(int index, [NotNullWhen(true)] out Weekday? value)
+            => (index >= 0 && index <= 6) 
+                ? (value = weekdays[index]) is not null 
+                : (value = null) is not null;
+
+        internal static bool TryGetByName(string weekdayName, [NotNullWhen(true)] out Weekday? value)
+            => (value = weekdays.FirstOrDefault(
+                    x => StringComparer.InvariantCultureIgnoreCase.Equals(x.Name.Trim(), weekdayName)
+                )) is not null;
+
+        private static readonly List<Weekday> weekdays;
+
+        static Weekdays()
+        {
+            var dayNames = CultureInfo.InvariantCulture.DateTimeFormat.DayNames;
+            weekdays= new List<Weekday>(dayNames.Length);
+            foreach (DayOfWeek dayOfWeek in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                weekdays.Add(new Weekday(
+                    ((int)dayOfWeek + 6) % 7
+                    , dayNames.Single(x =>
+                        StringComparer.InvariantCultureIgnoreCase.Equals(x
+                            , Enum.GetName(typeof(DayOfWeek), dayOfWeek)!
+                        )
+                    )
+                ));
+            }
+            weekdays = weekdays.OrderBy(x => x.Index).ToList();
+        }
+
+        public static Weekday Monday { get => weekdays[0]; }
+        public static Weekday Tuesday { get => weekdays[1]; }
+        public static Weekday Wednesday { get => weekdays[2]; }
+        public static Weekday Thursday { get => weekdays[3]; }
+        public static Weekday Friday { get => weekdays[4]; }
+        public static Weekday Saturday { get => weekdays[5]; }
+        public static Weekday Sunday { get => weekdays[6]; }
+    }
+
+}
