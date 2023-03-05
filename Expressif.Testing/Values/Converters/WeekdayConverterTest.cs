76bf8f2b00f04797dd777e4259eb5978856519ae
Cédric L. Charlier <seddryck@gmail.com>
2023-03-05 16:43:25 +0100

    Add tests for Weekday converter and fix issue when converting to string

--- Expressif.Testing/Values/Converters/WeekdayConverterTest.cs
+++ Expressif.Testing/Values/Converters/WeekdayConverterTest.cs
@@ -13,6 +13,22 @@ namespace Expressif.Testing.Values.Converters
     public class WeekdayConverterTest
     {
         [Test]
+        public void CanConvertFrom_Valid()
+            => Assert.That(
+                TypeDescriptor.GetConverter(typeof(Weekday)).CanConvertFrom(typeof(string))
+                , Is.True);
+
+        [Test]
+        [TestCase(typeof(int))]
+        [TestCase(typeof(decimal))]
+        [TestCase(typeof(bool))]
+        [TestCase(typeof(TimeOnly))]
+        public void CanConvertFrom_Invalid(Type type)
+            => Assert.That(
+                TypeDescriptor.GetConverter(typeof(Weekday)).CanConvertFrom(type)
+                , Is.False);
+
+        [Test]
         public void ConvertFromInvariantString_Valid()
             => Assert.That(
                 TypeDescriptor.GetConverter(typeof(Weekday)).ConvertFromInvariantString("Saturday")
@@ -24,5 +40,29 @@ namespace Expressif.Testing.Values.Converters
         public void ConvertFromInvariantString_Invalid(string value)
             => Assert.Throws<FormatException>(
                 () => TypeDescriptor.GetConverter(typeof(Weekday)).ConvertFromInvariantString(value));
+
+
+        [Test]
+        public void CanConvertTo_Valid()
+            => Assert.That(
+                TypeDescriptor.GetConverter(typeof(Weekday)).CanConvertTo(typeof(string))
+                , Is.True);
+
+        [Test]
+        [TestCase(typeof(int))]
+        [TestCase(typeof(decimal))]
+        [TestCase(typeof(bool))]
+        [TestCase(typeof(TimeOnly))]
+        public void CanConvertTo_Invalid(Type type)
+            => Assert.That(
+                TypeDescriptor.GetConverter(typeof(Weekday)).CanConvertTo(type)
+                , Is.False);
+
+
+        [Test]
+        public void ConvertToInvariantString_Valid()
+            => Assert.That(
+                TypeDescriptor.GetConverter(typeof(Weekday)).ConvertToInvariantString(Weekdays.Monday)
+                , Is.EqualTo("Monday"));
     }
 }
