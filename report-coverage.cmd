346605c198fb75987516f52b1d8f5b4e441ba66e
Cédric L. Charlier <seddryck@gmail.com>
2023-03-05 17:01:24 +0100

    Improve robustness of batch for report coverage

--- report-coverage.cmd
+++ report-coverage.cmd
@@ -1,3 +1,4 @@
+dotnet build Expressif.sln -c Release --nologo 
 dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover --no-build --nologo
 dotnet reportgenerator "-reports:Expressif.Testing\coverage.*.opencover.xml" "-targetdir:.\.coverage"
 start .coverage/index.html
