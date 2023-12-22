6dc37c636b721abab6feb7f1cf30b1ce9cdefea8
Cédric L. Charlier <seddryck@gmail.com>
2023-12-22 14:01:31 +0100

    feat: add functions to change year or month

---
+++ Expressif.Testing/Functions/Temporal/DatePartChangeFunctionsTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expressif.Functions.Temporal;
+using Expressif.Values;
+
+namespace Expressif.Testing.Functions.Temporal;
+public class DatePartChangeFunctionsTest
+{
+    [Test]
+    [TestCase(1900, 1903)]
+    [TestCase(2000, 2000)]
+    [TestCase(-45, 16)]
+    [TestCase(800, 803)]
+    [TestCase(12300, 2000)]
+    public void ChangeOfYear_Integer_Valid(int year, int expected)
+        => Assert.That(new ChangeOfYear(() => expected).Evaluate(year), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("1900-01-01", 1903, "1903-01-01")]
+    [TestCase("2000-01-01", 2000, "2000-01-01")]
+    [TestCase("800-01-01", 2000, "2000-01-01")]
+    [TestCase("2004-02-29", 2008, "2008-02-29")]
+    [TestCase("2004-02-29", 2005, "2005-02-28")]
+    public void ChangeOfYear_DateTime_Valid(DateTime dt, int newYear, DateTime expected)
+        => Assert.That(new ChangeOfYear(() => newYear).Evaluate(dt), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("1900-01", 1903, "1903-01")]
+    [TestCase("2000-01", 2000, "2000-01")]
+    [TestCase("0800-01", 2000, "2000-01")]
+    public void ChangeOfYear_YearMonth_Valid(YearMonth yearMonth, int newYear, YearMonth expected)
+        => Assert.That(new ChangeOfYear(() => newYear).Evaluate(yearMonth), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("1900-01-01", 3, "1903-01-01")]
+    [TestCase("2000-01-31", 3, "2000-03-31")]
+    [TestCase("2000-01-31", 4, "2000-04-30")]
+    [TestCase("2000-01-31", 2, "2000-02-29")]
+    [TestCase("2001-01-31", 2, "2001-02-28")]
+    public void ChangeOfMonth_DateTime_Valid(DateTime dt, int newMonth, DateTime expected)
+        => Assert.That(new ChangeOfMonth(() => newMonth).Evaluate(dt), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("1900-01-01", -1)]
+    [TestCase("1900-01-01", 0)]
+    [TestCase("1900-01-01", 13)]
+    public void ChangeOfMonth_DateTime_Invalid(DateTime dt, int newMonth)
+        => Assert.That(new ChangeOfMonth(() => newMonth).Evaluate(dt)!.Equals(null), Is.True);
+
+    [Test]
+    [TestCase("1900-01", 3, "1903-03")]
+    [TestCase("2000-01", 1, "2000-01")]
+    [TestCase("0800-01", 3, "0800-03")]
+    public void ChangeOfMonth_YearMonth_Valid(YearMonth yearMonth, int newMonth, YearMonth expected)
+        => Assert.That(new ChangeOfMonth(() => newMonth).Evaluate(yearMonth), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("1900-01", -1)]
+    [TestCase("1900-01", 0)]
+    [TestCase("1900-01", 13)]
+    public void ChangeOfMonth_YearMonth_Invalid(YearMonth yearMonth, int newMonth)
+        => Assert.That(new ChangeOfMonth(() => newMonth).Evaluate(yearMonth)!.Equals(null), Is.True);
+}
---
+++ Expressif/Functions/Temporal/DatePartChangeFunctions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expressif.Functions.Special;
+using Expressif.Values;
+using Expressif.Values.Casters;
+using Expressif.Values.Special;
+
+namespace Expressif.Functions.Temporal;
+public abstract class BaseDatePartChangeFunction : BaseTemporalFunction
+{
+    protected override object? EvaluateUncasted(object value)
+    {
+        if (new Null().Equals(value))
+            return EvaluateNull();
+
+        if (new IntegerCaster().TryCast(value, out var integer))
+            return EvaluateInteger(integer);
+
+        if (new YearMonthCaster().TryCast(value, out var yearMonth))
+            return EvaluateYearMonth(yearMonth);
+
+        if (new DateTimeCaster().TryCast(value, out var dateTime))
+            return EvaluateDateTime(dateTime);
+
+        return null;
+    }
+
+    protected virtual object? EvaluateInteger(int numeric) => null;
+    protected virtual object? EvaluateYearMonth(YearMonth yearMonth) => null;
+}
+
+/// <summary>
+/// returns a temporal value corresponding to the same day and month of the argument value but of the year passed as the parameter.
+/// If the original date was the 29th of February and the year passed as a parameter is not a leap year then it returns the 28th of February.
+/// </summary>
+public class ChangeOfYear : BaseDatePartNumericFunction
+{
+    public Func<int> Year { get; }
+    public ChangeOfYear(Func<int> year)
+        => Year = year;
+
+    protected override object EvaluateDateTime(DateTime value)
+    {
+        var newYear = Year.Invoke();
+        if (newYear < 1 || newYear > 9999)
+            return new Null();
+        var newDay = value.Month == 2 && value.Day == 29 && !DateTime.IsLeapYear(newYear) ? 28 : value.Day;
+        return new DateTime(newYear, value.Month, newDay, value.Hour, value.Minute, value.Second, value.Millisecond);
+    }
+    protected override object? EvaluateInteger(int numeric) => Year.Invoke();
+    protected override object? EvaluateYearMonth(YearMonth yearMonth) => new YearMonth(Year.Invoke(), yearMonth.Month);
+}
+
+/// <summary>
+/// returns a temporal value corresponding to the same day and year of the argument value but of the month passed as the parameter.
+/// If the original day is 29, 30, or 31 and the new month passed as a parameter has fewer days then it returns the last day of the corresponding month.
+/// </summary>
+public class ChangeOfMonth : BaseDatePartChangeFunction
+{
+    public Func<int> Month { get; }
+    public ChangeOfMonth(Func<int> month)
+        => Month = month;
+
+    protected override object EvaluateDateTime(DateTime value)
+    {
+        var newMonth = Month.Invoke();
+        if (newMonth < 1 || newMonth > 12)
+            return new Null();
+        var lastDayOfMonth = new DateTime(value.Year, newMonth, 1).AddMonths(1).AddDays(-1).Day;
+        var newDay = value.Day > lastDayOfMonth ? lastDayOfMonth : value.Day;
+        return new DateTime(value.Year, newMonth, newDay, value.Hour, value.Minute, value.Second, value.Millisecond);
+    }
+    protected override object? EvaluateYearMonth(YearMonth yearMonth)
+    {
+        var newMonth = Month.Invoke();
+        if (newMonth < 1 || newMonth > 12)
+            return new Null();
+        return new YearMonth(yearMonth.Year, newMonth);
+    }
+}
+
