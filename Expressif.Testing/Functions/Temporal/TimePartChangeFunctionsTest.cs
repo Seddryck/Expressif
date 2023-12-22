0701ef864c9503db286c65063b8f5fae873b0de8
Cédric L. Charlier <seddryck@gmail.com>
2023-12-22 18:57:53 +0100

    feat: functions to change a part of the time

---
+++ Expressif.Testing/Functions/Temporal/TimePartChangeFunctionsTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expressif.Functions.Temporal;
+using Expressif.Values;
+
+namespace Expressif.Testing.Functions.Temporal;
+public class TimePartChangeFunctionsTest
+{
+    [Test]
+    [TestCase("2001-01-01 17:12:16", 17, "2001-01-01 17:12:16")]
+    [TestCase("1901-01-01 05:04:26", 12, "1901-01-01 12:04:26")]
+    [TestCase("0800-01-01 12:00:00", 14, "0800-01-01 14:00:00")]
+    public void ChangeOfHour_DateTime_Valid(DateTime dt, int newYear, DateTime expected)
+        => Assert.That(new ChangeOfHour(() => newYear).Evaluate(dt), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("2001-01-01 17:12:16", -1)]
+    [TestCase("2001-01-01 17:12:16", 24)]
+    public void ChangeOfHour_YearMonth_Invalid(DateTime dt, int newMonth)
+        => Assert.That(new ChangeOfHour(() => newMonth).Evaluate(dt)!.Equals(null), Is.True);
+
+    [Test]
+    [TestCase("2001-01-01 17:12:16", 44, "2001-01-01 17:44:16")]
+    [TestCase("1901-01-01 05:04:26", 00, "1901-01-01 05:00:26")]
+    [TestCase("0800-01-01 12:00:00", 30, "0800-01-01 12:30:00")]
+    public void ChangeOfMinute_DateTime_Valid(DateTime dt, int newYear, DateTime expected)
+        => Assert.That(new ChangeOfMinute(() => newYear).Evaluate(dt), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("2001-01-01 17:12:16", -1)]
+    [TestCase("2001-01-01 17:12:16", 60)]
+    public void ChangeOfMinute_YearMonth_Invalid(DateTime dt, int newMonth)
+        => Assert.That(new ChangeOfMinute(() => newMonth).Evaluate(dt)!.Equals(null), Is.True);
+
+    [Test]
+    [TestCase("2001-01-01 17:12:16", 17, "2001-01-01 17:12:17")]
+    [TestCase("1901-01-01 05:04:26", 0, "1901-01-01 05:04:00")]
+    [TestCase("0800-01-01 12:00:00", 10, "0800-01-01 12:00:10")]
+    public void ChangeOfSecond_DateTime_Valid(DateTime dt, int newSecond, DateTime expected)
+        => Assert.That(new ChangeOfSecond(() => newSecond).Evaluate(dt), Is.EqualTo(expected));
+
+    [Test]
+    [TestCase("2001-01-01 17:12:16", -1)]
+    [TestCase("2001-01-01 17:12:16", 60)]
+    public void ChangeOfSecond_YearMonth_Invalid(DateTime dt, int newMonth)
+        => Assert.That(new ChangeOfSecond(() => newMonth).Evaluate(dt)!.Equals(null), Is.True);
+}
---
+++ Expressif/Functions/Temporal/TimePartChangeFunctions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expressif.Functions.Special;
+using Expressif.Values;
+using Expressif.Values.Casters;
+using Expressif.Values.Special;
+
+namespace Expressif.Functions.Temporal;
+public abstract class BaseTimePartChangeFunction : BaseTemporalFunction
+{ }
+
+/// <summary>
+/// returns a temporal value corresponding to the same instant of the argument value but with a new value for the second part.
+/// </summary>
+public class ChangeOfHour : BaseDatePartNumericFunction
+{
+    public Func<int> Hour { get; }
+    public ChangeOfHour(Func<int> hour)
+        => Hour = hour;
+
+    protected override object EvaluateDateTime(DateTime value)
+    {
+        var newHour = Hour.Invoke();
+        if (newHour < 0 || newHour > 23)
+            return new Null();
+        return new DateTime(value.Year, value.Month, value.Day, newHour, value.Minute, value.Second, value.Millisecond);
+    }
+}
+
+/// <summary>
+/// returns a temporal value corresponding to the same instant of the argument value but with a new value for the second part.
+/// </summary>
+public class ChangeOfMinute : BaseDatePartNumericFunction
+{
+    public Func<int> Minute { get; }
+    public ChangeOfMinute(Func<int> minute)
+        => Minute = minute;
+
+    protected override object EvaluateDateTime(DateTime value)
+    {
+        var newMinute = Minute.Invoke();
+        if (newMinute < 0 || newMinute > 59)
+            return new Null();
+        return new DateTime(value.Year, value.Month, value.Day, value.Hour, newMinute, value.Second, value.Millisecond);
+    }
+}
+
+/// <summary>
+/// returns a temporal value corresponding to the same instant of the argument value but with a new value for the second part.
+/// </summary>
+public class ChangeOfSecond : BaseDatePartNumericFunction
+{
+    public Func<int> Second { get; }
+    public ChangeOfSecond(Func<int> second)
+        => Second = second;
+
+    protected override object EvaluateDateTime(DateTime value)
+    {
+        var newSecond = Second.Invoke();
+        if (newSecond < 0 || newSecond > 59)
+            return new Null();
+        return new DateTime(value.Year, value.Month, value.Day, value.Hour, value.Minute, newSecond, value.Millisecond);
+    }
+}
