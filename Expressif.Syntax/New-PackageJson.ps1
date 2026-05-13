dba2130dc95dccf5bd1e71c0470cd36c4eb6a25e
Cédric L. Charlier <seddryck@gmail.com>
2026-05-14 00:49:31 +0200

    feat: syntax highlighter for Expressif

---
+++ Expressif.Syntax/New-PackageJson.ps1
@@ -0,0 +1,28 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $Version,
+
+    [Parameter(Mandatory = $false)]
+    [string] $OutputPath = ".\obj\package.json"
+)
+
+$model = [ordered]@{
+        version = $Version
+    }
+    | ConvertTo-Json -Depth 20
+
+Write-Host "Model sets version to value '$($model.version)'."
+
+Write-Host "Running Didot via local installation..."
+$model | dotnet tool run didot `
+    -t .\package.json.sbn `
+    -e scriban `
+    -i `
+    -r json `
+    -o $OutputPath `
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Didot execution failed"
+}
+
+Write-Host "Generated: $OutputPath"
