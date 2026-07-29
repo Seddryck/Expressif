d43c9980532cea2bd35becdaebe57f75e4bf6c1b
Cédric L. Charlier <seddryck@gmail.com>
2026-07-29 19:03:16 +0200

    feat: add set functions (difference, symmetric-difference, complement)
    * add difference, symmetric-difference, and complement array functions
    * introduce shared set-function base for common behavior
    * add array conformance YAML suites for all three operators
    * add consolidated conformance anchor tests in SetFunctionsTest
    * append function metadata entries in docs data JSON

---
+++ Expressif.Testing/Functions/Array/SetFunctionsTest.cs
@@ -0,0 +1,28 @@
+using Expressif.Functions.Array;
+using Expressif.Testing.Conformance;
+
+namespace Expressif.Testing.Functions.Array;
+
+[TestFixture]
+public class DifferenceTest
+{
+    [Conformance]
+    public void Difference_Valid_Array(object? value, object?[] array, object? expected)
+        => Assert.That(new Difference(() => array).Evaluate(value), Is.EqualTo(expected));
+}
+
+[TestFixture]
+public class SymmetricDifferenceTest
+{
+    [Conformance]
+    public void SymmetricDifference_Valid_Array(object? value, object?[] array, object? expected)
+        => Assert.That(new SymmetricDifference(() => array).Evaluate(value), Is.EqualTo(expected));
+}
+
+[TestFixture]
+public class ComplementTest
+{
+    [Conformance]
+    public void Complement_Valid_Array(object? value, object?[] array, object? expected)
+        => Assert.That(new Complement(() => array).Evaluate(value), Is.EqualTo(expected));
+}
