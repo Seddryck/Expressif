65f151ac05ca7da26a0dc9ae3a826f868c99147a
Cédric L. Charlier <seddryck@gmail.com>
2026-07-29 11:15:39 +0200

    refactor: use a base class for array functions

--- Expressif/Functions/Array/Fold.cs
+++ Expressif/Functions/Array/Fold.cs
@@ -1,6 +1,7 @@
-using Expressif.Functions;
-using Expressif.Accumulators;
 using System;
+using System.Collections;
+using Expressif.Accumulators;
+using Expressif.Functions;
 
 namespace Expressif.Functions.Array;
 
@@ -10,7 +11,7 @@ namespace Expressif.Functions.Array;
 /// Returns `null` when the input is not an enumerable or is a string.
 /// </summary>
 [Function]
-public class Fold : IFunction
+public class Fold : BaseArrayFunction
 {
     public Func<IAccumulator> Accumulator { get; }
 
@@ -26,11 +27,8 @@ public class Fold : IFunction
     public Fold(string accumulator)
         : this(() => AccumulatorFactory.Instantiate(accumulator)) { }
 
-    public object? Evaluate(object? value)
+    protected override object? EvaluateArray(IEnumerable enumerable)
     {
-        if (!AggregationEnumerable.TryGetEnumerable(value, out var enumerable))
-            return null;
-
         var accumulator = Accumulator.Invoke();
         accumulator.Initialize();
         foreach (var item in enumerable!)
