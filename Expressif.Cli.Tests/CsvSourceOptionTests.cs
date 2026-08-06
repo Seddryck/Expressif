d07ab9dc56f324a5be551c26e47719631c458598
Cédric L. Charlier <seddryck@gmail.com>
2026-08-06 17:20:34 +0200

    feat(cli): configure CSV source profiles

---
+++ Expressif.Cli.Tests/CsvSourceOptionTests.cs
@@ -0,0 +1,53 @@
+using Expressif.Cli.Commands;
+
+namespace Expressif.Cli.Tests;
+
+public class CsvSourceOptionTests
+{
+    [Test]
+    public void BuildCsvProfile_AllSupportedOptions_AreTranslated()
+    {
+        var (profile, hasHeader) = RunCommand.BuildCsvProfile(
+        [
+            "delimiter=\";\"", "line-terminator=\"|\"", "quote-char=null",
+            "double-quote=false", "escape-char=\"\\\\\"", "header=false",
+            "header-rows={1, 3}", "header-join=\".\"", "header-repeat=false",
+            "comment-char=\"#\"", "comment-rows={2, 4}", "null-sequence=\"NULL\"",
+            "missing-cell=\"missing\"", "skip-initial-space=true",
+            "array-delimiter=\";\"", "array-prefix=\"[\"", "array-suffix=\"]\""
+        ]);
+
+        var dialect = profile.Dialect;
+        Assert.Multiple(() =>
+        {
+            Assert.That(dialect.Delimiter, Is.EqualTo(';'));
+            Assert.That(dialect.LineTerminator, Is.EqualTo("|"));
+            Assert.That(dialect.QuoteChar, Is.Null);
+            Assert.That(dialect.DoubleQuote, Is.False);
+            Assert.That(dialect.EscapeChar, Is.EqualTo('\\'));
+            Assert.That(dialect.Header, Is.False);
+            Assert.That(hasHeader, Is.False);
+            Assert.That(dialect.HeaderRows, Is.EqualTo(new[] { 1, 3 }));
+            Assert.That(dialect.HeaderJoin, Is.EqualTo("."));
+            Assert.That(dialect.HeaderRepeat, Is.False);
+            Assert.That(dialect.CommentChar, Is.EqualTo('#'));
+            Assert.That(dialect.CommentRows, Is.EqualTo(new[] { 2, 4 }));
+            Assert.That(dialect.NullSequence, Is.EqualTo("NULL"));
+            Assert.That(dialect.MissingCell, Is.EqualTo("missing"));
+            Assert.That(dialect.SkipInitialSpace, Is.True);
+            Assert.That(dialect.ArrayDelimiter, Is.EqualTo(';'));
+            Assert.That(dialect.ArrayPrefix, Is.EqualTo('['));
+            Assert.That(dialect.ArraySuffix, Is.EqualTo(']'));
+        });
+    }
+
+    [TestCase("unknown=1", "Unknown CSV source option 'unknown' with value '1'.")]
+    [TestCase("delimiter=\"long\"", "Invalid CSV source option 'delimiter' with value '\"long\"'")]
+    [TestCase("header=\"true\"", "Invalid CSV source option 'header' with value '\"true\"'")]
+    [TestCase("header-rows={0}", "Invalid CSV source option 'header-rows' with value '{0}'")]
+    public void BuildCsvProfile_InvalidOption_IdentifiesNameAndValue(string option, string expected)
+    {
+        var exception = Assert.Throws<FormatException>(() => RunCommand.BuildCsvProfile([option]));
+        Assert.That(exception!.Message, Does.StartWith(expected));
+    }
+}
