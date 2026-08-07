3e0e9120f80bf55d95e0df45d693805acaea24ee
Cédric L. Charlier <seddryck@gmail.com>
2026-08-07 09:29:26 +0200

    fix CSV row materialization in run command

--- Expressif/Values/ContextObject.cs
+++ Expressif/Values/ContextObject.cs
@@ -25,6 +25,7 @@ public class ContextObject
         {
             DataRow row => index < row.Table.Columns.Count,
             ILiteDataRow row => index < row.ColumnCount,
+            RecordValue record => index >= 0 && index < record.Count,
             TupleValue tuple => index >= 0 && index < tuple.Count,
             IList list => index < list.Count,
             _ => throw new NotIndexableContextObjectException(Value)
@@ -38,6 +39,7 @@ public class ContextObject
             {
                 DataRow row => index < row.Table.Columns.Count ? row[index] : throw new ArgumentOutOfRangeException(index.ToString()),
                 ILiteDataRow row => index < row.ColumnCount ? row[index] : throw new ArgumentOutOfRangeException(index.ToString()),
+                RecordValue record => index >= 0 && index < record.Count ? record[index] : throw new ArgumentOutOfRangeException(index.ToString()),
                 TupleValue tuple => index >= 0 && index < tuple.Count ? tuple[index] : null,
                 IList list => list[index],
                 _ => throw new NotIndexableContextObjectException(Value)
