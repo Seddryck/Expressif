7f363d8e5c42c5113dfcb6be6d91f355f71dce14
Cédric L. Charlier <seddryck@gmail.com>
2026-08-19 22:31:57 +0200

    feat: add typed coercion functions

--- Expressif/Values/Casters/Caster.cs
+++ Expressif/Values/Casters/Caster.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Expressif.Values.Special;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,10 +23,12 @@ public class Caster
         {
             { typeof(object[]), () => new ArrayCaster().Cast(value) },
             { typeof(bool), () => new BooleanCaster().Cast(value) },
+            { typeof(DateOnly), () => new DateOnlyCaster().Cast(value) },
             { typeof(DateTime), () => new DateTimeCaster().Cast(value) },
             { typeof(int), () => new IntegerCaster().Cast(value) },
             { typeof(decimal), () => new NumericCaster().Cast(value) },
             { typeof(string), () => new TextCaster().Cast(value) },
+            { typeof(TimeOnly), () => new TimeOnlyCaster().Cast(value) },
             { typeof(YearMonth), () => new YearMonthCaster().Cast(value) },
         };
 
@@ -35,6 +39,48 @@ public class Caster
             { return ConvertTo<T>(value); }
             catch (Exception)
             { throw new ArgumentException($"Cannot convert the value '{value}' from a type '{value.GetType().Name}' to a '{typeof(T).Name}'"); }
+    }
+
+    public virtual bool TryCast<T>(object? value, [NotNullWhen(true)] out T? result)
+    {
+        result = default;
+        if (new Null().Equals(value) || new Empty().Equals(value) || new Whitespace().Equals(value))
+            return false;
+
+        if (value is T typed)
+        {
+            result = typed;
+            return true;
+        }
+
+        if (!TryCast(value!, typeof(T), out var converted))
+            return false;
+
+        result = (T)converted!;
+        return true;
+    }
+
+    private static bool TryCast(object value, Type targetType, out object? result)
+    {
+        result = null;
+        if (targetType == typeof(bool) && new BooleanCaster().TryCast(value, out var boolean))
+            result = boolean;
+        else if (targetType == typeof(DateOnly) && new DateOnlyCaster().TryCast(value, out var date))
+            result = date;
+        else if (targetType == typeof(DateTime) && new DateTimeCaster().TryCast(value, out var dateTime))
+            result = dateTime;
+        else if (targetType == typeof(int) && new IntegerCaster().TryCast(value, out var integer))
+            result = integer;
+        else if (targetType == typeof(decimal) && new NumericCaster().TryCast(value, out var numeric))
+            result = numeric;
+        else if (targetType == typeof(string) && new TextCaster().TryCast(value, out var text))
+            result = text;
+        else if (targetType == typeof(TimeOnly) && new TimeOnlyCaster().TryCast(value, out var time))
+            result = time;
+        else
+            return false;
+
+        return true;
     }
 
     protected virtual T? ConvertTo<T>(object value)
