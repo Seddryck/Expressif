244bfe6f2fadca60230026ce91f4c6a45cd72c5f
Cédric L. Charlier <seddryck@gmail.com>
2026-08-19 12:02:52 +0200

    chore: update Expressif.Syntax to 0.20.0

--- Expressif/Bindings/ExpressifBinder.cs
+++ Expressif/Bindings/ExpressifBinder.cs
@@ -206,7 +206,7 @@ public sealed class ExpressifBinder
         IntervalLiteralSyntax interval => new IntervalParameter(BindInterval(interval)),
         ArrayLiteralSyntax array => new ArrayParameter(array.Values.Select(BindArgument).ToArray()),
         TupleLiteralSyntax tuple => new TupleParameter(tuple.Values.Select(BindValue).ToArray()),
-        RecordLiteralSyntax record => new RecordLiteralParameter(record.Fields.Select(field => new RecordLiteralField(field.Name, BindValue(field.Value))).ToArray()),
+        RecordLiteralSyntax record => new RecordLiteralParameter(record.Fields.Select(field => new RecordLiteralField(field.Name.Value, BindValue(field.Value))).ToArray()),
         _ => throw Unsupported(syntax),
     };
 
