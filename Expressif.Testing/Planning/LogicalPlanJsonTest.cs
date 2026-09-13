ea0f1d26a9509733825ecb92c308f1a5a9b8fdcb
Cédric L. Charlier <seddryck@gmail.com>
2026-09-13 18:30:09 +0200

    test(planning): cover logical plan branches

--- Expressif.Testing/Planning/LogicalPlanJsonTest.cs
+++ Expressif.Testing/Planning/LogicalPlanJsonTest.cs
@@ -51,6 +51,79 @@ public class LogicalPlanJsonTest
     }
 
     [Test]
+    public void Serialize_AllSupportedLiteralRuntimeTypes_WritesExpectedJsonValues()
+    {
+        LogicalValue[] values =
+        [
+            new LogicalLiteral("null", null),
+            new LogicalLiteral("boolean", true),
+            new LogicalLiteral("text", "value"),
+            new LogicalLiteral("decimal", 12.5m),
+            new LogicalLiteral("integer", 12),
+            new LogicalLiteral("integer", 13L),
+            new LogicalLiteral("date", new DateOnly(2026, 9, 13)),
+            new LogicalLiteral("datetime", new DateTime(2026, 9, 13, 12, 30, 0, DateTimeKind.Utc)),
+            new LogicalLiteral("time", new TimeOnly(12, 30, 1, 250)),
+            new LogicalLiteral("duration", TimeSpan.FromMinutes(90)),
+        ];
+
+        using var document = JsonDocument.Parse(LogicalPlanJson.Serialize(
+            new LogicalPlan(new LogicalPipeline(values)),
+            indented: false));
+        var serialized = document.RootElement.GetProperty("plan").GetProperty("items")
+            .EnumerateArray().Select(item => item.GetProperty("value").ToString()).ToArray();
+
+        Assert.That(serialized, Is.EqualTo(new[]
+        {
+            string.Empty, "True", "value", "12.5", "12", "13", "2026-09-13",
+            "2026-09-13T12:30:00.0000000Z", "12:30:01.2500000", "01:30:00",
+        }));
+    }
+
+    [Test]
+    public void Serialize_UnsupportedLiteralRuntimeType_ThrowsFormatException()
+    {
+        var plan = new LogicalPlan(new LogicalPipeline([new LogicalLiteral("custom", new Version(1, 2))]));
+
+        var exception = Assert.Throws<LogicalPlanFormatException>(() => LogicalPlanJson.Serialize(plan));
+
+        Assert.That(exception!.Message, Is.EqualTo("Literal type 'custom' contains unsupported value 'Version'."));
+    }
+
+    [Test]
+    public void Deserialize_InvalidJson_WrapsJsonException()
+    {
+        var exception = Assert.Throws<LogicalPlanFormatException>(() => LogicalPlanJson.Deserialize("{"));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(exception!.Message, Is.EqualTo("The logical-plan document is not valid JSON."));
+            Assert.That(exception.InnerException, Is.InstanceOf<JsonException>());
+        });
+    }
+
+    [TestCase("boolean", "\"not-a-boolean\"", typeof(InvalidOperationException))]
+    [TestCase("date", "\"not-a-date\"", typeof(FormatException))]
+    [TestCase("integer", "9223372036854775808", typeof(FormatException))]
+    [TestCase("duration", "\"999999999999999999999.00:00:00\"", typeof(OverflowException))]
+    public void Deserialize_InvalidLiteralValue_WrapsConversionException(
+        string type,
+        string value,
+        Type expectedInnerException)
+    {
+        var json = "{\"format\":\"expressif.logical-plan\",\"version\":1,\"catalogCompatibility\":\"3.0\"," +
+            $"\"plan\":{{\"kind\":\"pipeline\",\"items\":[{{\"kind\":\"literal\",\"type\":\"{type}\",\"value\":{value}}}]}}}}";
+
+        var exception = Assert.Throws<LogicalPlanFormatException>(() => LogicalPlanJson.Deserialize(json));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(exception!.Message, Is.EqualTo("The logical-plan document contains an invalid value."));
+            Assert.That(exception.InnerException, Is.TypeOf(expectedInnerException));
+        });
+    }
+
+    [Test]
     public void Deserialize_UnsupportedVersion_ReportsSupportedVersion()
     {
         var json = LogicalPlanJson.Serialize(Plan("trim"), indented: false)
